428dda24f174c430c574979aabae2b32b99a260e
varunt <torkavarun@gmail.com>
2026-02-18 13:18:41 +0800

    feat: upgrade @sentry/nextjs to v10 and remove webpack config block

--- next.config.mjs
+++ next.config.mjs
@@ -71,18 +71,6 @@ export default withSentryConfig(
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
 
-    // Webpack-specific options
-    webpack: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      treeshake: { removeDebugLogging: true },
-
-      // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    },
-
     // Disable Sentry telemetry
     telemetry: false,
   },
