2c951ef53dbe10c4f81fb02eae17106727a51525
varunt <torkavarun@gmail.com>
2026-07-01 09:49:30 +0800

    feat(config): add site-config as single brand source of truth

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,9 +1,11 @@
 import type { LocalePrefixMode } from 'next-intl/routing';
 
+import { SITE_CONFIG } from '@/config/site-config';
+
 const localePrefix: LocalePrefixMode = 'as-needed';
 
 export const AppConfig = {
-  name: 'VT SaaS Template',
+  name: SITE_CONFIG.brand.name,
   locales: [
     {
       id: 'en',
